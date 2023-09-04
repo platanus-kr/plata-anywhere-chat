b3d691ce823aaccf9d810e7c8f01f50a247f0675
platanus <platanus.kr@gmail.com>
2023-09-04 17:16:46 +0900

    refactor #24: (web) AuthService ing..

--- web/src/main/java/org/platanus/platachat/web/auth/config/SecurityFilterChainConfig.java
+++ web/src/main/java/org/platanus/platachat/web/auth/config/SecurityFilterChainConfig.java
@@ -7,6 +7,8 @@ import org.platanus.platachat.web.auth.app.PasswordEncoderConfig;
 import org.platanus.platachat.web.auth.oauth2.CustomOAuth2UserService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
@@ -69,10 +71,10 @@ public class SecurityFilterChainConfig {
 //        return new ProviderManager(provider);
 //    }
 
-//    @Bean
-//    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
-//        return authenticationConfiguration.getAuthenticationManager();
-//    }
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
+    }
 //
     @Bean
     public CustomAuthenticationSuccessHandler customAuthenticationSuccessHandler() {
--- web/src/main/java/org/platanus/platachat/web/auth/service/AuthServiceImpl.java
+++ web/src/main/java/org/platanus/platachat/web/auth/service/AuthServiceImpl.java
@@ -8,7 +8,6 @@ import org.platanus.platachat.web.auth.dto.SessionMemberDto;
 import org.platanus.platachat.web.member.dto.MemberLoginRequestDto;
 import org.platanus.platachat.web.member.model.Member;
 import org.platanus.platachat.web.member.service.MemberService;
-import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
@@ -21,22 +20,28 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class AuthServiceImpl implements AuthService {
     private final AuthenticationProvider authenticationProvider;
+    //    private final AuthenticationManager authenticationManager;
     private final MemberService memberService;
 
     @Override
     public SessionMemberDto authorizationSession(HttpServletRequest request, HttpSession session, MemberLoginRequestDto dto) {
+
+        // TODO: 여기서 AuthenticationManager, UserDetailService 다 호출해서 해결하기.
         Member member;
         try {
             member = memberService.login(dto.getUsername(), dto.getPassword());
         } catch (IllegalArgumentException e) {
             throw new IllegalArgumentException(e);
         }
+        Authentication authentication = getAuthentication(member.getUsername(), member.getPassword());
+
+
         UsernamePasswordAuthenticationToken token = new UsernamePasswordAuthenticationToken(member.getUsername(), member.getPassword());
-        Authentication authentication = authenticationProvider.authenticate(token);
+//        Authentication authentication = authenticationProvider.authenticate(token);
 
         SecurityContextHolder.getContext().setAuthentication(authentication);
-        session.setAttribute(HttpSessionSecurityContextRepository.SPRING_SECURITY_CONTEXT_KEY,
-                SecurityContextHolder.getContext());
+//        SecurityContextHolder.getContext().setAuthentication(token);
+        session.setAttribute(HttpSessionSecurityContextRepository.SPRING_SECURITY_CONTEXT_KEY, SecurityContextHolder.getContext());
         session.setAttribute("member", new SessionMemberDto(member, request.getSession().getId()));
         return new SessionMemberDto(member, session.getId());
     }
